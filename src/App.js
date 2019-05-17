e4a330f0ebdf348a6045f5abc751bea8c1f7b17a
Norkute <Milda.Norkute@thomsonreuters.com>
2019-05-17 14:19:47 +0200

    loading data in progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import * as d3 from 'd3';
+//import '../data/grafikas'
 
 class App extends Component {
   constructor(props) {
@@ -8,22 +9,32 @@ class App extends Component {
     this.state={temperatureData: [ 8, 5, 13, 9, 12 ]}
   }
 
+
 componentDidMount(){
+  d3.csv("../data/grafikas.csv", function(data) {
+    for (var i = 0; i < 3; i++){
+      console.log(data[i].Isvezimo plano ID);
+    }
+  });
+
   d3.select(this.refs.temperatures)
   .selectAll("h2")
   .data(this.state.temperatureData)
   .enter()
       .append("h2")
-      .text("New Temperature")
+      .text("New Temperature")    
 }
-  componentDidUpdate(){
-    d3.select(this.refs.temperatures)
-      .selectAll("h2")
-      .data(this.state.temperatureData)
-      .enter()
-          .append("h2")
-          .text("New Temperature")
-  }
+
+componentDidUpdate(){
+  d3.select(this.refs.temperatures)
+    .selectAll("h2")
+    .data(this.state.temperatureData)
+    .enter()
+        .append("h2")
+        .text("New Temperature")
+}
+
+
 
   render(){
   return(
