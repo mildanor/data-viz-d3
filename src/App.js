33c7a458f6bb73a452fa9d31ba710fe58fbf75b1
Milda Norkute <mildanorkute@mildas-mbp.ten.thomsonreuters.com>
2019-08-30 16:23:28 +0200

    I drew France and printed my data yay

--- src/App.js
+++ src/App.js
@@ -2,59 +2,73 @@ import React, {Component} from 'react';
 import './App.css';
 import * as d3 from 'd3';
 
+//single country exmaple https://www.d3-graph-gallery.com/graph/backgroundmap_country.html
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state={data: []}
+    this.state={
+      data: [],
+      geography: {}
+    }
   }
 
 
 componentDidMount(){
-
   var json = require("./data/csvjson.json");
   //console.log(json);
      let jsonNew= this.state.data;
-  for (var i = 0; i < 3; i++) {
+  for (var i = 0; i < 4; i++) {
     var jsonItem = json[i].IsvezimoplanoID;
-    //console.log(jsonNew);
+    console.log(jsonNew);
      jsonNew.push([jsonItem]);
           this.setState({
             data: jsonNew,
           });
   }
+var svg = d3.select("svg")
+var width = +svg.attr("width");
+var height = +svg.attr("height");
 
-  /*
-  d3.json(json, function(data) {
-    console.log(data);
-  })
-  */
-  
+var projection = d3.geoMercator()
+    .center([2, 47])                // GPS of location to zoom on
+    .scale(1080)                       // This is like the zoom
+    .translate([ width/2, height/2 ])
+
+d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
+  .then(function(data){
+    // Code from your callback goes here...
+    data.features = data.features.filter(function(d){/*console.log(d.properties.name)*/ ; return d.properties.name==="France"})
+   // console.log(data.features)
+   // Draw the map
+    svg.append("g")
+        .selectAll("path")
+        .data(data.features)
+        .enter()
+        .append("path")
+          .attr("fill", "grey")
+          .attr("d", d3.geoPath()
+              .projection(projection)
+          )
+        .style("stroke", "none")
+  });
 
   d3.select(this.refs.temperatures)
   .selectAll("h2")
   .data(this.state.data)
   .enter()
       .append("h2")
-      .text(this.state.data)    
+      .text(this.state.data)  
+   
 }
-
-/*
-componentDidUpdate(){
-  d3.select(this.refs.temperatures)
-    .selectAll("h2")
-    .data(this.state.temperatureData)
-    .enter()
-        .append("h2")
-        .text("New Temperature")
-}
-*/
-
 
 
   render(){
   return(
-    <div ref="temperatures"></div>
+    <div ref="temperatures">
+    <h2></h2>
+   <svg id="my_dataviz" width="440" height="300"></svg>
+    </div>
   )
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Example from './App2';
+//import Example from './App2';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
